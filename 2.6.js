e73de8dbd2f14f3e5535664830f24a4bd118d1f4
haimaco <102148627+haimaco@users.noreply.github.com>
2022-04-15 15:04:22 +0300

    Create 2.6.js

---
+++ 2.6.js
@@ -0,0 +1,17 @@
+//2.6 Years and Centuries
+//The first century spans from the year 1 up to and including the year 100, The second - from the
+//year 101 up to and including the year 200, etc.
+//Task :
+//Given a year, return the century it is in.
+//Input , Output Examples ::
+//centuryFromYear(1705) returns (18)
+//centuryFromYear( 1900) returns (19)
+//centuryFromYear(1601) returns (17)
+//centuryFromYear(2000) returns (20)
+
+const Century= (year)=>{
+    return year %100  ? Math.floor(year / 100) + 1 : year / 100;
+
+}
+console.log(Century(1998));
+console.log(Century(2022));

fe75c489d6a083e9991a8992b62d909ac186b95e
haimaco <102148627+haimaco@users.noreply.github.com>
2022-04-15 15:00:56 +0300

    Create 1.1.js

---
+++ 1.1.js
@@ -0,0 +1,14 @@
+# JS-Weekend-exercise
+//1.1 YES OR NOT :
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No"
+//string for false.
+
+const booleanvalue =(bool)=>
+{
+        let boolean ;
+        bool ? boolean = "Yes" : boolean = "No";
+        return boolean;
+    
+};
+
+console.log(booleanvalue(false));
